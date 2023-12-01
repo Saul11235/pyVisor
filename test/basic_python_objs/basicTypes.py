7e9dd733e58b064d72210a544a67463f004d6b8e
Edwin Saul <edwinsaulpm@gmail.com>
2023-11-30 19:28:54 -0500

    final changes on footer

---
+++ test/basic_python_objs/basicTypes.py
@@ -0,0 +1,19 @@
+from pyVisor import visor
+
+class basicTypes:
+    type_None=None
+    type_bool=bool
+    type_int=int
+    type_float=float
+    type_strig=str
+    type_list=list
+    type_tuple=tuple 
+    type_dict=dict
+    
+
+
+
+
+v=visor(basicTypes,"basicTypes")
+v.run()
+
